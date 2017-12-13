bceaf6288453cff1e04d5392c0e30799d46615c9
shawnhr <shawnk723@live.com>
2017-12-13 14:55:25 -0800

    completed minimum req

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var arr = [];
+  var nodeCheck = function(node){
+  	if(node.classList && node.classList.contains(className)){
+  		arr.push(node);
+  	}
+  	if(node.hasChildNodes){
+  		for(var i = 0; i < node.childNodes.length; i++){
+  			nodeCheck(node.childNodes[i]);
+  		}
+  	}
+  };
+  nodeCheck(document.body);
+  return arr;  
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+
+  if( Array.isArray(obj) ){
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr.join(',') + ']';
+  }
+  if (obj && typeof obj === 'object'){
+    var arr = [];
+    for( var key in obj) {
+      if (obj[key] === undefined || typeof(obj[key]) === 'function'){
+        continue;
+      }
+      arr.push(stringifyJSON(key)+ ":" + stringifyJSON(obj[key]));
+    }
+    return '{' + arr.join(',') + '}';
+  }
+
+  if( typeof obj === 'string' ){
+   return '"' + obj + '"';
+  }
+
+  return '' + obj;
 };
