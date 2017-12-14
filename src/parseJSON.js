6f56f249283c3f9ecbf3941a78b3e1625174d819
shawnhr <shawnk723@live.com>
2017-12-13 16:14:09 -0800

    ParseJSON partially done

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,51 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  //null
+  if(json === 'null'){
+  	return null;
+  }
+  //boolean(true)
+  if(json === 'true'){
+  	return true;
+  }
+  //boolean(false)
+  if(json === 'false'){
+  	return false;
+  }
+  //undefined
+  if(json === 'undefined'){
+  	return undefined;
+  }
+  //number
+  if(Number(json)!== NaN){
+  	return Number(json);
+  }
+  //string
+  var arr = json.split('');
+  if(arr[0] === '\"'){
+  	return arr.slice(1, arr.length -1).join(''); 
+  }
+  //array
+  if(arr[0] === '['){
+  	var str = arr.slice(1, arr.length -1).join('');
+  	var parArr = str.split(',');
+  	for(var i = 0; i < parArr.length; i++){
+  		parArr[i] = parseJSON(parArr[i]);
+  	}
+  	return parArr;
+  }
+  //obj
+  if(arr[0] === '{'){
+  	var obj = {};
+  	var str = arr.slice(1, arr.length -1).join('');
+  	var parArr = str.split(',');
+  	var result = parArr.split(':');
+  	for(var i =0; i < result.length;i++){
+  		obj[parseJSON(result[i])] = parseJSON(result[i+1]);
+  	}
+  	return obj;
+  }
+
+
 };
